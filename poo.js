de29924e8811ac4ff36c9875a4ea8e36906eeeca
juandap123 <juandavidcardozo374@gmail.com>
2026-03-04 14:12:37 -0500

    combo familiar

--- poo.js
+++ poo.js
@@ -36,11 +36,34 @@ console.log(pizzapeperoni.empacar());
 
 
 class combos extends Pizza {
-    constructor(combo , cantidad , tamano , masa , ingrediente, bebida, postre)  {
+    constructor(combo , cantidad , tamano , masa , ingrediente, bebidas, postres)  {
         super(masa, tamano, ingrediente);
         this.combo = combo
-        this.cantidad =  cantidad
+        this.cantidad = cantidad
         this.bebidas = bebidas
         this.postres = postres 
     }
-}
+
+    elegircombo() {
+        this.preparar();
+        this.hornear();
+        this.empacar();
+
+        console.log(
+            `has elegido el combo ${this.combo} que incluye ${this.cantidad} pizzas de tamano ${this.tamano} con masa 
+            ${this.masa} y los siguientes ingredientes: ${this.ingredientes}. ademas, incluye una bebida de ${this.
+               bebida} y un postre de ${this.postre}.`
+        );
+        return this;     
+    
+            
+    };
+
+
+};
+
+
+
+const combofamiliar = new combos("familiar", 4, "grande", "delgada", ["jamon", "piña", "queso"], "refresco", "helado");
+console.log(combofamiliar.elegircombo()); 
+
