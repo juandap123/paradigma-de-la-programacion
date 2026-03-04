f04f67d74340cbe7ae98938c76180795bb92190b
juandap123 <juandavidcardozo374@gmail.com>
2026-03-04 16:15:46 -0500

    funcion

--- funciones.js
+++ funciones.js
@@ -7,10 +7,31 @@ function suma(x) {
 console.log(suma(7));
 
 
-function division(x){
+function hola(x){
     let accion = x + 5
     return accion; 
 
 };
 
 console.log(division(5));
+
+let resultado = 0;
+function SumarPrecios(a, b) {
+    return resultado = resultado + a * b;
+   
+};
+
+console.log(SumarPrecios(4, 6));
+console.log(SumarPrecios(2, 3));
+console.log(SumarPrecios(5, 8));
+
+
+ function Multiplicar(a) {
+    return a * a;
+};
+
+function SumaDeLaMultiplicacion(num1, num2,) {
+    return Multiplicar (num1) + Multiplicar(num2);
+};
+
+console.log(SumaDeLaMultiplicacion(3, 4));
