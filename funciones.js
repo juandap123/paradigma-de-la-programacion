5d59176c8f7d0ace9b349eae81bdcc1e67c2f4cf
juandap123 <juandavidcardozo374@gmail.com>
2026-03-04 15:35:13 -0500

    funciones

---
+++ funciones.js
@@ -0,0 +1,16 @@
+function suma(x) {
+    let accion = x + 5;
+    return accion;
+
+};
+
+console.log(suma(7));
+
+
+function division(x){
+    let accion = x + 5
+    return accion; 
+
+};
+
+console.log(division(5));
